623fc9be9a13980f0c40b7f70cce5b0878ce44dc
Marcus Watts <mwatts@redhat.com>
2025-05-10 13:05:50 -0400

    fix to harmonize with python ssl server side changes

--- kmip/services/server/server.py
+++ kmip/services/server/server.py
@@ -287,17 +287,22 @@ class KmipServer(object):
         for cipher in auth_suite_ciphers:
             self._logger.debug(cipher)
 
-        self._socket = ssl.wrap_socket(
-            self._socket,
-            keyfile=self.config.settings.get('key_path'),
+        self._ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+
+        self._ctx.load_cert_chain(
             certfile=self.config.settings.get('certificate_path'),
+            keyfile=self.config.settings.get('key_path')
+        )
+        self._ctx.set_ciphers(self.auth_suite.ciphers)
+        self._ctx.load_verify_locations(cafile=self.config.settings.get('ca_path'))
+        self._ctx.verify_mode = ssl.CERT_REQUIRED
+#            ssl_version=self.auth_suite.protocol,
+
+        self._socket = self._ctx.wrap_socket(
+            self._socket,
             server_side=True,
-            cert_reqs=ssl.CERT_REQUIRED,
-            ssl_version=self.auth_suite.protocol,
-            ca_certs=self.config.settings.get('ca_path'),
             do_handshake_on_connect=False,
-            suppress_ragged_eofs=True,
-            ciphers=self.auth_suite.ciphers
+            suppress_ragged_eofs=True
         )
 
         try:
